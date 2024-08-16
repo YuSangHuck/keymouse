494ad001eeeac40a6af9ffa201d3268068a08924
yusanghuck <g01099387636@gmail.com>
2024-08-17 03:44:02 +0900

    log 보강(파일 추가)

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 # 모드 상수
 MODE_KEYBOARD = 'keyboard'
@@ -30,11 +31,21 @@ mouse_move_value = 30
 mouse_scroll_value = 30
 
 # 로거 설정
-def setup_logger():
+def setup_logger(log_file='logs/application.log'):
+    # 로그 디렉토리 생성
+    log_dir = os.path.dirname(log_file)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d - %(funcName)s()] - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+
+    # 파일 핸들러 설정
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d - %(funcName)s()] - %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    # 핸들러 추가
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
