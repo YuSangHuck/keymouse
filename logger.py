30e49d43ca7379677f326f6afab6319b78d2803b
yusanghuck <g01099387636@gmail.com>
2024-08-17 13:07:49 +0900

    startup log 추가

--- logger.py
+++ logger.py
@@ -29,16 +29,30 @@ class SingletonLogger:
         # 파일 핸들러 설정
         file_handler = logging.FileHandler(log_file_path)
         file_handler.setLevel(logging.DEBUG)
-        file_formatter = logging.Formatter('[%(asctime)s] [%(module).9s::%(funcName).9s:%(lineno).3d] [%(levelname).1s] %(message)s')
-        file_handler.setFormatter(file_formatter)
+        default_formatter = logging.Formatter('[%(asctime)s] [%(module).9s::%(funcName).9s:%(lineno).3d] [%(levelname).1s] %(message)s')
+        file_handler.setFormatter(default_formatter)
 
         # 핸들러 추가
         logger.addHandler(file_handler)
 
         self.logger = logger
+        self.file_handler = file_handler
+        self.default_formatter = default_formatter
 
     def get_logger(self):
         return self.logger
+
+    def plain(self, log_message):
+        try:
+            # 포맷터를 %(message)s로 설정
+            plain_formatter = logging.Formatter('%(message)s')
+            self.file_handler.setFormatter(plain_formatter)
+
+            # 로그 메시지 기록
+            self.logger.info(log_message)
+        finally:
+            # 원래 포맷터로 복구
+            self.file_handler.setFormatter(self.default_formatter)
 
 # logger 인스턴스 생성
 logger_instance = SingletonLogger().get_logger()
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import signal
 import keyboard
+import sys
 from mode_manager import check_switch
 from config import KEY_SWITCH
+from logger import logger_instance, SingletonLogger
 
 if __name__ == "__main__":
+    startup_log = (
+"################################################################################\n"
+"#                               Program started                                #\n"
+"################################################################################"
+)
+    SingletonLogger().plain(startup_log)
+
     keyboard.add_hotkey(KEY_SWITCH, check_switch)
     keyboard.wait()
