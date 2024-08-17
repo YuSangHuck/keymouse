8d7900a3793a80a66137ee9bc41e949f9afdf428
yusanghuck <g01099387636@gmail.com>
2024-08-17 10:37:35 +0900

    logger 수정(exe 실행 강제, exe 경로기준으로 로그파일 생성)

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 
 class SingletonLogger:
@@ -11,16 +12,22 @@ class SingletonLogger:
         return cls._instance
 
     def _init_logger(self, log_file='logs/application.log'):
+        # 스크립트로 실행된 경우 예외를 발생시킴. PyInstaller로 빌드 강제
+        if not getattr(sys, 'frozen', False):
+            raise RuntimeError("This script can only be run as an executable.")
+
+        base_dir = os.path.dirname(sys.executable)
+        # 로그 파일의 절대 경로를 생성
+        log_file_path = os.path.join(base_dir, log_file)
         # 로그 디렉토리 생성
-        log_dir = os.path.dirname(log_file)
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
+        log_dir = os.path.dirname(log_file_path)
+        os.makedirs(log_dir, exist_ok=True)
 
         logger = logging.getLogger('KeyMouseLogger')
         logger.setLevel(logging.DEBUG)
 
         # 파일 핸들러 설정
-        file_handler = logging.FileHandler(log_file)
+        file_handler = logging.FileHandler(log_file_path)
         file_handler.setLevel(logging.DEBUG)
         file_formatter = logging.Formatter('[%(asctime)s] [%(module).9s::%(funcName).9s:%(lineno).3d] [%(levelname).1s] %(message)s')
         file_handler.setFormatter(file_formatter)
